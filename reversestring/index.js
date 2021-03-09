7e1c88f19e9cef38fc31a02d52b1a2361020fee0
Emmanuel Egomson <fegomson@gmail.com>
2021-03-09 13:53:07 -0600

    refactored code

--- reversestring/index.js
+++ reversestring/index.js
@@ -7,11 +7,6 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-    // Solution 3
-    // Using the reduce helper method
-    // convert the given str into an array, use the reduce helper method
-    // return the result
-
     const newStr = str.split("").reduce((rev, char) => char + rev, '')
     return newStr
 }
@@ -21,18 +16,20 @@ reverse("asdf");
 module.exports = reverse;
 
 
-// // Solution 1
-    // const newStr = str.split("").reverse().join("")
-    // return newStr
+// Solution 1
+    // return str.split("").reverse().join("")
 
-
-// // Solution 2 - Seems to take longer
-    // For Loop
+    // Solution 2
     // const newStr = str.split("")
     // const reversedStr = []
-    // for (let i = 0; i < newStr.length; i++) {
-    //     const char = newStr[i]
-    //     reversedStr.unshift(char)
-    // }
 
+    // for(let i = 0; i < newStr.length; i++){
+    //   let char = newStr[i]
+    //   reversedStr.unshift(char)
+    // }
     // return reversedStr.join("")
+
+
+    // Solution 3
+    // const newStr = str.split("").reduce((rev, char) => char + rev, '')
+    // return newStr
