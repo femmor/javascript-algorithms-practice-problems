26a16f27dc39130e255091a2023802a4d3509120
Emmanuel Egomson <fegomson@gmail.com>
2021-03-09 13:45:20 -0600

    Reverse String Problem - 3 possible solutions

--- reversestring/index.js
+++ reversestring/index.js
@@ -7,33 +7,32 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-    // // Solution 1
-    // // convert the given str to an array
-    // // use the reverse helper method to reverse the array
-    // // join the array back into a str with the join method
-    // return str.split("").reverse().join("");
-
-
-    // // Solution 2 - Seems to take longer
-    // // Using forOf loop
-    // // create a new empty string
-    // let reversed = ""
-    // // for each char in the the given str, add the char to the empty reversed string
-    // for (const char of str) {
-    //     reversed = char + reversed
-    // }
-    // // return the reversed string
-    // return reversed
-
-
     // Solution 3
     // Using the reduce helper method
     // convert the given str into an array, use the reduce helper method
     // return the result
-    return str.split("").reduce((rev, char) => char + rev, "");
 
+    const newStr = str.split("").reduce((rev, char) => char + rev, '')
+    return newStr
 }
 
 reverse("asdf");
 
 module.exports = reverse;
+
+
+// // Solution 1
+    // const newStr = str.split("").reverse().join("")
+    // return newStr
+
+
+// // Solution 2 - Seems to take longer
+    // For Loop
+    // const newStr = str.split("")
+    // const reversedStr = []
+    // for (let i = 0; i < newStr.length; i++) {
+    //     const char = newStr[i]
+    //     reversedStr.unshift(char)
+    // }
+
+    // return reversedStr.join("")
