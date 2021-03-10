fadad261af18b579b67068afaff6b7b813421f00
Emmanuel Egomson <fegomson@gmail.com>
2021-03-10 17:01:01 -0600

    FizzBuzz problem

--- fizzbuzz/index.js
+++ fizzbuzz/index.js
@@ -14,20 +14,13 @@
 
 function fizzBuzz(n) {
     for (let i = 1; i <= n; i++) {
-        // is the number a multiple of 3 and 5?
-        if (i % 15 === 0 && i % 5 === 0) {
-            console.log("fizzbuzz")
-        } 
-        // is the number a multiple of 3?
-        else if (i % 3 === 0) {
-            console.log("fizz")
-        }
-        // is the number a multiple of 5?
-        else if (i % 5 === 0) {
-            console.log("buzz")
-        }
-        // else just print the number
-        else {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log('fizzbuzz')
+        } else if(i % 3 === 0){
+            console.log('fizz')
+        } else if(i % 5 === 0){
+            console.log('buzz')
+        } else {
             console.log(i)
         }
     }
