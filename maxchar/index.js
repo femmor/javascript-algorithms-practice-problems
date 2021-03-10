b5ee15fa268997226fee3ec211cecc55c864ba4d
Emmanuel Egomson <fegomson@gmail.com>
2021-03-10 16:51:14 -0600

    maxChar problem

--- maxchar/index.js
+++ maxchar/index.js
@@ -6,37 +6,21 @@
 // maxChar("apple 1231111") === "1"
 
 function maxChar(str) {
-    // set empty object to take in characters
     const charMap = {}
-    // initial maximum occurrence of character
     let max = 0
-    // character with the maximum occurrence
-    let maxChar = ""
+    let maxChar = ''
 
-    // iterate through str and add to charMap the object/key values of most occurring characters
     for (let char of str) {
-        // if character count occurs
-        if(charMap[char]) {
-            // increment
-            charMap[char]++
-        } else {
-            // else set character occurrence = 1
-            charMap[char] = 1
-        }
+        charMap[char] = charMap[char] + 1 || 1
     }
 
-    // iterate through charMap and return the char with most occurrence
     for (let char in charMap) {
-        // check if character occurrence is greater than max
         if (charMap[char] > max) {
-            // set max = that number of occurrence
             max = charMap[char]
-            // set maxChar = the character with most occurrence
             maxChar = char
         }
     }
 
-    // return maxChar
     return maxChar
 }
 
