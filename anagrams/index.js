155dd234279868c2b62ea80afd82ae0919219d92
Emmanuel Egomson <fegomson@gmail.com>
2021-03-11 16:05:46 -0600

    Anagrams problem

--- anagrams/index.js
+++ anagrams/index.js
@@ -8,8 +8,45 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {
-    
+// Solution 2
+function anagrams(stringA, stringB){
+  return cleanString(stringA) === cleanString(stringB)
 }
 
+function cleanString(str){
+  return str.replace(/[^\w]/g, "").split("").sort().join("")
+}
+
+
 module.exports = anagrams;
+
+
+// Solution 1
+// function anagrams(stringA, stringB) {
+//   const aCharMap = buildCharMap(stringA)
+//   const bCharMap = buildCharMap(stringB)
+
+//   if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+//     return false
+//   }
+
+//   for (let char in aCharMap) {
+//     if(aCharMap[char] !== bCharMap[char]){
+//       return false
+//     }
+//   }
+
+//   return true
+// }
+
+// function buildCharMap(str){
+//   let charMap = {}
+
+//   for (let char of str) {
+//     charMap[char] = charMap[char] + 1 || 1
+//   }
+
+//   return charMap
+// }
+
+
