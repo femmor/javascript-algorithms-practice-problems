30e6359b56892145821ade8d1756140e988588a2
Emmanuel Egomson <fegomson@gmail.com>
2021-03-17 14:18:23 -0500

    Finding vowels problem

--- vowels/index.js
+++ vowels/index.js
@@ -7,6 +7,41 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+// Solution 3
+function vowels(str) {
+  const matches = str.match(/[aeiou]/gi)
+  return matches ? matches.length : 0
+}
 
 module.exports = vowels;
+
+
+// Solution 1
+// function vowels(str) {
+//   let count = 0
+//   const vowels = 'aeiou' 
+//   const newStr = str.toLowerCase()
+
+//   for(let char of newStr){
+//     for(let i = 0; i < vowels.length; i++) {
+//       char === vowels[i] ? count += 1 : null
+//     }
+//   }
+
+//   return count
+// }
+
+
+
+// Solution 2
+// function vowels(str) {
+//   let count = 0
+//   const checker = ['a', 'e', 'i', 'o', 'u']
+//   const newStr = str.toLowerCase()
+
+//   for(let char of newStr){
+//     checker.includes(char) ? count++ : null
+//   }
+
+//   return count
+// }
